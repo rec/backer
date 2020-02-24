70b15607bfbc7276dc93a0ba92a2cbcf5fe1f444
Tom Ritchford <tom@swirly.com>
2020-02-24 14:13:11 +0100

    Simplify backer/tasks.py slightly

--- backer/tasks.py
+++ backer/tasks.py
@@ -1,15 +1,19 @@
+from . import database, git, rsync
 import inspect
-import importlib
 
 
-def _task(name):
-    mod = importlib.import_module('backer.' + name)
-    sig = inspect.signature(mod.run)
-    params = sig.parameters
-    defaults = {k: v.default for k, v in params.items()}
-    mod.DEFAULTS = {k: v for k, v in defaults.items() if v is not sig.empty}
-    return mod
+def _make():
+    tasks, defaults = {}, {}
+    for module in database, git, rsync:
+        name = module.__name__.split('.')[-1]
+        tasks[name] = module
+
+        sig = inspect.signature(module.run)
+        params = sig.parameters
+        d = {k: v.default for k, v in params.items()}
+        defaults[name] = {k: v for k, v in d.items() if v is not sig.empty}
+
+    return tasks, defaults
 
 
-TASKS = {k: _task(k) for k in ('database', 'git', 'rsync')}
-DEFAULTS = {k: v.DEFAULTS for k, v in TASKS.items()}
+TASKS, DEFAULTS = _make()
