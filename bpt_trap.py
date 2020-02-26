4c271de27d5bed799d5758d00559cd666410345e
Tom Ritchford <tom@swirly.com>
2020-02-26 17:35:03 +0100

    Minimal reproducible example

--- bpt_trap.py
+++ bpt_trap.py
@@ -1,39 +1,15 @@
-import threading
-import time
-from watchdog.observers import Observer
 from pathlib import Path
-from queue import Queue, Empty
-import os
+from watchdog.observers import Observer
 
 
 def main():
-    observer = Observer()
-    queue = Queue()
-    source = Path(os.path.expandvars('.')).expanduser().resolve()
-
-    def items_in_queue():
-        items = []
-        while True:
-            try:
-                items.append(queue.get(block=False))
-            except Empty:
-                return items
-
-    def service_queue():
-        while True:
-            if items_in_queue():
-                time.sleep(0.05)
-                items_in_queue()
-
-    threading.Thread(target=service_queue, daemon=True).start()
-
     class Handler:
         @staticmethod
         def dispatch(event):
-            if not event.is_directory:
-                queue.event(event)
+            print(event)
 
-    observer.schedule(Handler(), source, recursive=True)
+    observer = Observer()
+    observer.schedule(Handler(), Path('.'), recursive=True)
     observer.start()
 
 
