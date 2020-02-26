86faf7e99519c06e507af8dd12dcd67b922f45c4
Tom Ritchford <tom@swirly.com>
2020-02-26 17:59:34 +0100

    Modify bpt_trap.py

--- bpt_trap.py
+++ bpt_trap.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 from watchdog.observers import Observer
 
+WANT_TO_FAIL = True
+
+if WANT_TO_FAIL:
+    PATH = Path('.')
+else:
+    PATH = '.'
+
 
 def main():
     class Handler:
@@ -9,9 +16,13 @@ def main():
             print(event)
 
     observer = Observer()
-    observer.schedule(Handler(), Path('.'), recursive=True)
-    observer.start()
+    observer.schedule(Handler(), PATH, recursive=True)
 
+    observer.start()
+    # If PATH is a path, the line above causes:
+    # Compilation trace/BPT trap: 5 at Wed Feb 26 17:38:13
+    # on Python 3.6.6, Python 3.7.0, Python 3.8.1
+    # Darwin bantam.local 16.7.0 Darwin Kernel Version 16.7.0: (etc)
 
 if __name__ == '__main__':
     main()
