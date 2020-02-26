883d4e4887d2c3499d88042059eafbf5dcda1072
Tom Ritchford <tom@swirly.com>
2020-02-26 17:00:00 +0100

    Several changes
    Modify backer/__main__.py
    Delete backer/tasks.py

--- backer/__main__.py
+++ backer/__main__.py
@@ -1,28 +1,19 @@
-from . import tasks
+from . import git
 from .execute import Execute
 
 
 def main(args=None):
     cfg = {
-        'git': {
-            '0': {
-                'target': None,
-                'source': None,
-                'remotes': None,
-                'git_init': True,
-                'add_unknown_files': True,
-                'file_event_window': 0.05,
-                'commit_message': '%Y-%m-%dT%H:%M%SZ',
-            }
-        }
+        'target': None,
+        'source': None,
+        'remotes': None,
+        'git_init': True,
+        'add_unknown_files': True,
+        'file_event_window': 0.05,
+        'commit_message': '%Y-%m-%dT%H:%M%SZ',
     }
     execute = Execute()
-
-    for task_name, section in cfg.items():
-        task = tasks.TASKS[task_name]
-        for name, desc in section.items():
-            task.run(execute, name, **desc)
-
+    git.run(execute, 'one', **cfg)
     execute.start()
 
 
