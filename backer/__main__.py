a7a7ff0d993adff34b80827d806e0590f2578dd8
Tom Ritchford <tom@swirly.com>
2020-02-26 17:12:10 +0100

    It passes

--- backer/__main__.py
+++ backer/__main__.py
@@ -1,9 +1,73 @@
-from . import git
 import schedule as _schedule
 import subprocess
 import threading
 import time
 from watchdog.observers import Observer
+from pathlib import Path
+from queue import Queue, Empty
+import os
+
+
+def run(execute, name, target=None, source=None,
+        remotes=None,
+        git_init=True,
+        add_unknown_files=True,
+        file_event_window=0.05,
+        commit_message='%Y-%m-%dT%H:%M%SZ'):
+    """"
+    `git commit` automatically on any change within a directory.
+
+    Whenever any file changes within the `source` directory recursively,
+    attempt to make a git commit, and if this succeeds, push it to all remotes.
+
+    name:
+      name of git backup
+
+    source:
+      source directory to be backed up (default is current working directory)
+
+    target:
+      (not used)
+
+    remotes:
+      A dictionary mapping remote names to remote URLs
+
+    git_init:
+      If `source` is not a Git repository, then if `git_init` is true, then
+      `git init` will be called, otherwise an ValueError is raised
+
+    add_unknown_files:
+      If True, unknown files are automatically `git add`'ed
+
+    file_event_window:
+      If `file_event_window` is non-zero, then all file events during that
+      time window (in seconds) are consolidated into a single git commit
+
+    commit_message:
+      A strftime-style format string for commit messages
+    """
+    queue = Queue()
+    remotes = remotes or {}
+    source = Path(os.path.expandvars(source or '.')).expanduser().resolve()
+
+    def items_in_queue():
+        items = []
+        while True:
+            try:
+                items.append(queue.get(block=False))
+            except Empty:
+                return items
+
+    def service_queue():
+        while True:
+            if items_in_queue():
+                if file_event_window:
+                    time.sleep(file_event_window)
+                    items_in_queue()
+                pass
+
+    threading.Thread(target=service_queue, daemon=True).start()
+    execute.observe(queue.put, source.absolute())
 
 
 class Execute:
@@ -69,7 +133,7 @@ def main(args=None):
         'commit_message': '%Y-%m-%dT%H:%M%SZ',
     }
     execute = Execute()
-    git.run(execute, 'one', **cfg)
+    run(execute, 'one', **cfg)
     execute.start()
 
 
