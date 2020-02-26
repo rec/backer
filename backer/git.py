0863203799ab1a8360a75de92cf88ecd7d35e3d9
Tom Ritchford <tom@swirly.com>
2020-02-26 17:01:55 +0100

    Modify backer/git.py

--- backer/git.py
+++ backer/git.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 from queue import Queue, Empty
-import datetime
-import functools
 import os
 import threading
 import time
@@ -46,7 +44,6 @@ def run(execute, name, target=None, source=None,
       A strftime-style format string for commit messages
     """
     queue = Queue()
-    git = functools.partial(execute.run, 'git', cwd=source)
     remotes = remotes or {}
     source = Path(os.path.expandvars(source or '.')).expanduser().resolve()
 
@@ -64,24 +61,7 @@ def run(execute, name, target=None, source=None,
                 if file_event_window:
                     time.sleep(file_event_window)
                     items_in_queue()
-                commit()
-
-    def commit():
-        lines = git('status', '--porcelain')
-        if not add_unknown_files:
-            lines = [i for i in lines if not i.startwith('??')]
-
-        if lines:
-            files = [i.split(maxsplit=1)[1] for i in lines]
-            git('add', *files)
-
-            if '%' in commit_message:
-                msg = datetime.datetime.now().strftime(commit_message)
-            else:
-                msg = commit_message
-            git('commit', '-am', msg)
-            for remote in remotes:
-                git('push', remote)
+                pass
 
     threading.Thread(target=service_queue, daemon=True).start()
     execute.observe(queue.put, source.absolute())
