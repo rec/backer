c495fe5483bac8a461d8a024a60ccdbef8ad8de4
Tom Ritchford <tom@swirly.com>
2020-03-02 13:27:58 +0100

    Improve test_mysql and fix a bug it found

--- test/test_mysql.py
+++ test/test_mysql.py
@@ -3,9 +3,18 @@ from . import patch
 
 @patch.execute
 class TestMySql(patch.MainTester):
-    def test_mysql(self):
+    def test_mysql_all(self):
         config = {'mysql': {'0': DATABASE}}
         self.run_test(config, MYSQL, '--all-databases')
+
+    def test_mysql_databases(self):
+        config = {'mysql': {'0': dict(DATABASE, databases='foo bar')}}
+        self.run_test(config, MYSQL, '--databases', 'foo', 'bar')
+
+    def test_mysql_database_and_tables(self):
+        sub = dict(DATABASE, databases='foo', tables=('bing', 'bang'))
+        config = {'mysql': {'0': sub}}
+        self.run_test(config, MYSQL, 'foo', 'bing', 'bang')
 
 
 DATABASE = {
