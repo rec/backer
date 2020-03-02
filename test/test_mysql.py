5b4caab25db45cd808ee5a6dabb519429c0bdba0
Tom Ritchford <tom@swirly.com>
2020-03-02 13:18:18 +0100

    Modify test/patch.py, test/test_mysql.py

--- test/test_mysql.py
+++ test/test_mysql.py
@@ -1,27 +1,18 @@
 from . import patch
 
-from tempfile import TemporaryDirectory
-import yaml
-
 
 @patch.execute
 class TestMySql(patch.MainTester):
     def test_mysql(self):
-        with TemporaryDirectory() as target:
-            cfg = yaml.safe_dump({'mysql': DATABASE})
-            with self.main(target, '-c', cfg) as ex:
-                assert ex is not None
-                ((cmd, *_), ) = ex.runs
-                assert list(cmd) == MYSQL.format(tmpdir=target).split()
+        config = {'mysql': {'0': DATABASE}}
+        self.run_test(config, MYSQL, '--all-databases')
 
 
 DATABASE = {
-    '0': {
-        'user': 'test_user',
-        'password': 'test_password',
-        'host': 'test_host',
-        'port': 7777,
-    }
+    'user': 'test_user',
+    'password': 'test_password',
+    'host': 'test_host',
+    'port': 7777,
 }
 
 MYSQL = """\
@@ -31,5 +22,4 @@ mysqldump\
  --port=7777\
  --host=test_host\
  --result-file={tmpdir}/0/mysql.sql\
- --all-databases\
 """
